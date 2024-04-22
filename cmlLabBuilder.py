99d5a912325f1f37120de7ceec21c16fc25e4c34
jc3943 <jcomer@cisco.com>
2024-04-22 13:46:09 -0400

    added topo yaml read, post to create lab, and lab start for nxos-vxlan-evpn topology

--- cmlLabBuilder.py
+++ cmlLabBuilder.py
@@ -12,6 +12,7 @@ import csv
 
 API_BASE_URL = "https://172.16.14.40"
 API_AUTH_PATH = "/api/v0/authenticate"
+API_LAB_IMPORT = "/api/v0/import"
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -20,6 +21,7 @@ headers = {
     "Accept": "application/json",
     "Content-Type": "application/json"
 }
+authDict = {}
 
 @click.command()
 @click.option("--cml_ip", type=str, help='CML IP Address', required=False)
@@ -33,13 +35,29 @@ def buildCmlTopo(cml_ip, topo):
     cmlAuthPayload = {'username':username, 'password':password}
     response = requests.post(authUrl, headers=headers, json=cmlAuthPayload, verify=False)
     cmlToken = response.json()
-    print(cmlToken)
+    tokenHdrValue = "Bearer " + cmlToken
+    headers['Authorization'] = tokenHdrValue
+
     #read topology yaml and create json payload for api post
     with open(topo, 'r') as topoIn:
         topoYaml = yaml.safe_load(topoIn)
-        print(json.dumps(topoYaml))
+        topJsonStr = json.dumps(topoYaml)
+        #print(json.dumps(topoYaml))
 
     #post to cml and raise exception on error response
+    labCreateUrl = API_BASE_URL + API_LAB_IMPORT
+    topoCreate = requests.post(labCreateUrl, headers=headers, data=topJsonStr, verify=False)
+    topoCreatJson = topoCreate.json()
+    labId = str(topoCreate.json()['id'])
+    print(labId)
+
+    #start the created lab
+    labStartUrl = API_BASE_URL + "/api/v0/labs/" + labId + "/start"
+    labStartResponse = requests.put(url=labStartUrl, headers=headers, verify=False)
+
+    #check node status for given lab and start any nodes that failed to start
+    
+
 
 
 if __name__ == "__main__":
