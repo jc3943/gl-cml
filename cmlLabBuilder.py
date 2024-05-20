edbe7f0a3f78100d94d979ff4f88dbb5bbb914f6
jc3943 <jcomer@cisco.com>
2024-05-20 19:35:13 -0400

    added cml topo node status/wipe/restart routine, updated gitlab-ci to build cml top, updated nso playbooks for correct docker host url

--- cmlLabBuilder.py
+++ cmlLabBuilder.py
@@ -7,7 +7,7 @@ import json, yaml
 import urllib3
 import urllib.parse
 import hvac
-import os, warnings
+import os, warnings, time
 import csv
 
 #export VAULT_ADDR='ip address for vault'
@@ -59,6 +59,33 @@ def buildCmlTopo(cml_ip, topo):
     labStartResponse = requests.put(url=labStartUrl, headers=headers, verify=False)
 
     #check node status for given lab and start any nodes that failed to start
+    nodeStatusUrl = API_BASE_URL + "/api/v0/nodes/"
+    nodeBooted = []
+    for i in range(0, 20):
+        nodeStatusResponse = requests.get(url=nodeStatusUrl, headers=headers, verify=False).json()
+        for k in range(len(nodeStatusResponse)):
+            nodeId = nodeStatusResponse[k]['id']
+            if (nodeStatusResponse[k]['state'] == "STOPPED"):
+                nodeWipeUrl = API_BASE_URL + f'/api/v0/labs/{labId}/nodes/{nodeId}/wipe_disks'
+                nodeWipeResponse = requests.put(url=nodeWipeUrl, headers=headers, verify=False)
+                print("Node Wipe: ", nodeWipeResponse, nodeId)
+                time.sleep(10)
+                nodeStartUrl = API_BASE_URL + f'/api/v0/labs/{labId}/nodes/{nodeId}/state/start'
+                nodeStartResponse = requests.put(url=nodeStartUrl, headers=headers, verify=False)
+                print("Node Start ", nodeStartResponse, nodeId)
+            elif (nodeStatusResponse[k]['state'] == "STARTED"):
+                print("Starting Lab: ", labId)
+                time.sleep(60)
+                break
+            elif (nodeStatusResponse[k]['state'] == "BOOTED" and nodeId not in nodeBooted):
+                nodeBooted.append(nodeId)
+        if (len(nodeBooted) == len(nodeStatusResponse)):
+            print("Lab and Node Start Complete!!")
+            break
+
+
+    
+
     
 
 
